6c7f39cb4dd1bafc06eb313466fa7882827ae416
Alperen Toksöz <aetoksoz@gmail.com>
2020-02-17 14:29:32 +0300

    Initial Commit

---
+++ SnapChatFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapChatFinal
+//
+//  Created by Alperen Toksöz on 17.02.2020.
+//  Copyright © 2020 Alperen Toksöz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
